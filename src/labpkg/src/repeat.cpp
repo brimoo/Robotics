d04220c6c84c1ceb39e8dc70b49d3337fa83139f
Brian Moore <bmoore6@ucmerced.edu>
2018-02-14 20:22:31 -0800

    Started lab 4: completed listener and started repeat

---
+++ src/labpkg/src/repeat.cpp
@@ -0,0 +1,35 @@
+#include <ros/ros.h>
+#include <tf2_ros/transform_listener.h>
+#include <geometry_msgs/TransformStamped.h>
+
+int main(int argc, char** argv){
+
+    // Initialize node
+    ros::init(argc, argv, "repeat");
+    ros::NodeHandle nh;
+
+    // Set up the transform listener
+    tf2_ros::Buffer buffer;
+    tf2_ros::TransformListener listener(buffer);
+    
+    // Use a geometry message to store the transform
+    geometry_msgs::TransformStamped transform;
+
+    // Enter main loop
+    while(nh.ok()){
+
+        // Try to listen for the transform
+        try{
+            transform = buffer.lookupTransform("husky_beta/base_link", "husky_alpha/base_link", ros::Time(0));
+        }
+        catch(tf2::TransformException &ex){
+            ROS_WARN("%s", ex.what());
+            ros::Duration(1.0).sleep();
+            continue;
+        }
+
+    }
+
+    return 0;
+
+}
